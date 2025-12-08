0150a5da9002625de470bfe4b1312d632a0da11b
palkamarlene <marlene.palka@zalf.de>
2025-12-08 15:36:20 +0100

    Define paths for running monica

--- projects/single_run/run_monica.cmd
+++ projects/single_run/run_monica.cmd
@@ -1,4 +1,4 @@
 rem set MONICA_PARAMETERS environment variable to the location of the monica-parameters folder
-set MONICA_PARAMETERS=..\..\monica-parameters
+set MONICA_PARAMETERS=C:\Users\palka\GitHub\MONICA_parameter_comparison\monica-parameters
 rem start monica with the simulation setup file (sim.json)
-..\..\bin\monica-run.exe -o .\sim-out.csv sim.json
+C:\Users\palka\GitHub\MONICA_parameter_comparison\bin\monica-run.exe -o .\sim-out.csv sim.json
